08b096aa67ea32eafdd592bf0b72be1e06fad9e5
steven <steven.zg@outlook.com>
2025-04-18 20:32:11 +1200

    refactor: remove process instances

--- src/services/user_role.ts
+++
@@ -1,7 +0,0 @@
-import { UserRole } from '../types/process';
-
-export async function fetchUserRoles(): Promise<UserRole[]> {
-  const res = await fetch('/api/user_roles');
-  if (!res.ok) throw new Error('Failed to fetch user roles');
-  return res.json();
-}
